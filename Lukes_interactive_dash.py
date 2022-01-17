6a828b426ac0918d78ef0ee30e16b84f9f429010
andrewhbashorm <c00238900@itcarlow.ie>
2022-01-17 11:48:14 +0000

    dash links

--- Lukes_interactive_dash.py
+++ Lukes_interactive_dash.py
@@ -1,14 +1,7 @@
+import dash_auth
 import dash
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
-import plotly.graph_objs as go
-import pandas as pd
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
-import dash_auth
-import dash
-
 from dash.dependencies import Input, Output, State
 import plotly.graph_objs as go
 
@@ -22,130 +15,21 @@ USERNAME_PASSWORD_PAIRS = [
     ['JamesBond', '007'], ['LouisArmstrong', 'satchmo']
 ]
 
-
-
-
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
 
+# Subset dataframe to show some specific columns in dash web app
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/'
-    'cb5392c35661370d95f300086accea51/raw/'
-    '8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/'
-    'indicators.csv')
-
+#51.56569468252655, -0.40344721931807725, 67_Lynmouth_Dr_Ruislip_HA4_9BY_UK
 lat_center = 53.34035434171382
 long_center = -6.189352520464214
 geo_coord_str = "Lat: " + str(lat_center) +", Long:" + str(long_center)
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(external_stylesheets=['https://codepen.io/amyoshino/pen/jzXypZ.css'])
+app.title = 'Open Street Map'
 
-
-app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-
-index_page = html.Div([
-    dcc.Link('Go to Page 1', href='/page-1'),
-    html.Br(),
-    dcc.Link('Go to Page 2', href='/page-2'),
-])
-
-
-
-
-available_indicators = df['Indicator Name'].unique()
-
-
-finderLayout = html.Div([
-    html.Div([
-        html.Div([
-            dcc.Dropdown(
-                id='xaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Fertility rate, total (births per woman)'
-            ),
-            dcc.RadioItems(
-                id='xaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],
-        style={'width': '48%', 'display': 'inline-block'}),
-
-        html.Div([
-            dcc.Dropdown(
-                id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='Life expectancy at birth, total (years)'
-            ),
-            dcc.RadioItems(
-                id='yaxis-type',
-                options=[{'label': i, 'value': i} for i in ['Linear', 'Log']],
-                value='Linear',
-                labelStyle={'display': 'inline-block'}
-            )
-        ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
-    ]),
-
-    dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
-        id='year--slider',
-        min=df['Year'].min(),
-        max=df['Year'].max(),
-        value=df['Year'].max(),
-        step=None,
-        marks={str(year): str(year) for year in df['Year'].unique()}
-    )
-], style={'padding':10})
-
-@app.callback(
-    Output('indicator-graphic', 'figure'),
-    [Input('xaxis-column', 'value'),
-     Input('yaxis-column', 'value'),
-     Input('xaxis-type', 'value'),
-     Input('yaxis-type', 'value'),
-     Input('year--slider', 'value')])
-def update_graph(xaxis_column_name, yaxis_column_name: object,
-                 xaxis_type, yaxis_type,
-                 year_value):
-    dff = df[df['Year'] == year_value]
-    return {
-        'data': [go.Scatter(
-            x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
-            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
-            text=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'],
-            mode='markers',
-            marker={
-                'size': 15,
-                'opacity': 0.5,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
-        )],
-        'layout': go.Layout(
-            xaxis={
-                'title': xaxis_column_name,
-                'type': 'linear' if xaxis_type == 'Linear' else 'log'
-            },
-            yaxis={
-                'title': yaxis_column_name,
-                'type': 'linear' if yaxis_type == 'Linear' else 'log'
-            },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
-            hovermode='closest'
-        )
-    }
-
-
-mapLayout = html.Div(
+app.layout = html.Div(
     html.Div([
         html.H1(children='Load Map from Address'),
         html.H3('Enter address: (67 Lynmouth Drive Ruislip HA4 9BY)', style={'paddingRight': '30px'}),
@@ -248,18 +132,5 @@ def update_map2(value):
         marker={'size': 10}))
     return figure
 
-@app.callback(dash.dependencies.Output('page-content', 'children'),
-              [dash.dependencies.Input('url', 'pathname')])
-
-
-
-def display_page(pathname):
-    if pathname == '/page-1':
-        return finderLayout
-    elif pathname == '/page-2':
-        return mapLayout
-    else:
-        return index_page
-
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
